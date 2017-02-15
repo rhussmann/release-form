425240ed55a85efe736b2840576fdc9d65fa94c0
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-02-15 07:17:47 -0500

    Modifying step definitions for the form
    * Tests are failing
    * Added selenium navigation to the tests

--- features/step_definitions/form_steps.js
+++ features/step_definitions/form_steps.js
@@ -1,23 +1,39 @@
-var seleniumWebdriver = require('selenium-webdriver');
-var {defineSupportCode} = require('cucumber');
+const chai = require('chai');
+const seleniumWebdriver = require('selenium-webdriver');
+const {defineSupportCode} = require('cucumber');
+
+const By = seleniumWebdriver.By;
+chai.should();
+
 const app = require('../../src/app');
 
 defineSupportCode(function({Given, When, Then}) {
   Given('I am on the form webpage', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    const that = this;
+    const driver = this.driver;;
     app.launch(() => {
-      callback(null, that.driver.get('http://localhost:3000/'));
+      callback(null, driver.get('http://localhost:3000/'));
     });
   });
 
-  When('I enter my data and click {stringInDoubleQuotes}', function (buttonName, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+  When('I enter my data and click {stringInDoubleQuotes}', function (buttonName) {
+    return this.driver.findElement(By.id('submit-button')).then((button) => {
+      return button.click();
+    });
   });
 
+  // When fetching our PDF in Chrome, the page source will have an element
+  // in the body named <embed> with an attribute of type="application/pdf"
+  //
+  // That is how to detect for a PDF (not cross-browser compatible)
   Then('I am presented with a PDF version of my form', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+
+    const driver = this.driver;
+    driver.findElement(By.id('plugin')).then((element) => {
+      return element.getAttribute('type');
+    }).then((typeValue) => {
+      typeValue.should.equal('application/pdf');
+      callback();
+    });
   });
+
 });
